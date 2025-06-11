cdf7a50ca715454a78803ee15afea6786a00c0bc
saraiva142 <saraivajandrade@gmail.com>
2025-06-11 16:10:30 -0300

    conexão com o docker para criar o servidor mysql na porta 3306, só dar docker-compose up

---
+++ src/test/java/joao/saraiva/agregadorinvestimentos/AgregadorinvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package joao.saraiva.agregadorinvestimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregadorinvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
