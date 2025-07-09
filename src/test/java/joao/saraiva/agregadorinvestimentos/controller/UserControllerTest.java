f2db537a332cde93a149b59e71df5935ce8a7133
saraiva142 <saraivajandrade@gmail.com>
2025-07-09 13:57:41 -0300

    Começando testes para Controller 🚀

---
+++ src/test/java/joao/saraiva/agregadorinvestimentos/controller/UserControllerTest.java
@@ -0,0 +1,28 @@
+package joao.saraiva.agregadorinvestimentos.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void listUsers() {
+    }
+
+    @Test
+    void updateUserById() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
